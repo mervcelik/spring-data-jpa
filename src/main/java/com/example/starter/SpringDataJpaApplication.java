9e9591117173ecdecce53147b7f58634e2d1b66f
mervcelik <mrv.ozturrk@gmail.com>
2026-07-01 17:12:54 +0300

    @Value, @ConfigurationProperty

--- src/main/java/com/example/starter/SpringDataJpaApplication.java
+++ src/main/java/com/example/starter/SpringDataJpaApplication.java
@@ -2,10 +2,14 @@ package com.example.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.example.configuration.GlobalProperties;
+
+@EnableConfigurationProperties(value=GlobalProperties.class)
 @EnableJpaRepositories(basePackages = {"com.example"})
 @ComponentScan(basePackages =  {"com.example"})
 @EntityScan(basePackages =  {"com.example"})
